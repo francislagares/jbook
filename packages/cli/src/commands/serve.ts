e0adf063f781d3bcbfed922af0c0d0a438c6b7e2
Francis Lagares <francis.lagares@gmail.com>
2021-02-09 17:23:05 +0100

    feat: implement error handling on server init

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -6,7 +6,17 @@ export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run sesrver on', '4005')
-  .action((filename = 'notebook.js', options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename))
-    serve(parseInt(options.port), path.basename(filename), dir)
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(`Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`);
+    } catch (error) {
+      if (error.code === 'EADDRINUSE') {
+        console.error('Port is in use. Try running on a different port.');
+      } else {
+        console.log('Here is the problem', error.message);
+      }
+      process.exit(1);
+    }
   });
